96c65e367a46c39f8ae0739fb400aeee91e4a1dd
Muqsit Rayyan <hackhack05@gmail.com>
2015-11-14 09:53:12 +0300

    Update Test.php

--- src/Muqsit/test/Test.php
+++ src/Muqsit/test/Test.php
@@ -1,1 +1,20 @@
 
+<?php
+
+namespace Muqsit\test;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\command\ComamndSender;
+
+class MyPlugin extends PluginBase{
+}
+
+    public function onCommand(CommandSender $sender, Command $command, $label, array $args){
+    $commandName = $command->getName();
+    if($commandName === "helpme"){
+      $sender->sendMessage("§bUsage: §e/helpme <vote|donate>");
+      return true;
+    }
+    return false;
+  }
